caf15de9dc1624d9a8f2b0bc034bf3b19c745737
Lurantis <1510033076@qq.com>
2024-09-20 10:30:59 +0800

    chore: :fire: remove useless code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,29 +16,26 @@ const raster = new TileLayer({
   }),
 })
 
-// 起点终点
 const keyPoints = ref([
-  [-74.0060, 40.7128], // 纽约市，美国
-  [-99.1332, 19.4326], // 墨西哥城，墨西哥
+  [-74.0060, 40.7128],
+  [-99.1332, 19.4326],
 ])
-// 途经点
 const path = ref([
-  [151.2093, -33.8688], // 悉尼，澳大利亚
-  [-0.1278, 51.5074], // 伦敦，英国
-  [18.4241, -33.9249], // 开普敦，南非
-  [139.6917, 35.6895], // 东京，日本
+  [151.2093, -33.8688],
+  [-0.1278, 51.5074],
+  [18.4241, -33.9249],
+  [139.6917, 35.6895],
 ])
 const path2 = [
-  [37.6173, 55.7558], // 莫斯科，俄罗斯
-  [-58.3816, -34.6037], // 布宜诺斯艾利斯，阿根廷
-  [2.3522, 48.8566], // 巴黎，法国
-  [151.2093, -33.8688], // 悉尼，澳大利亚
-  [-0.1278, 51.5074], // 伦敦，英国
-  [18.4241, -33.9249], // 开普敦，南非
-  [139.6917, 35.6895], // 东京，日本
+  [37.6173, 55.7558],
+  [-58.3816, -34.6037],
+  [2.3522, 48.8566],
+  [151.2093, -33.8688],
+  [-0.1278, 51.5074],
+  [18.4241, -33.9249],
+  [139.6917, 35.6895],
 ]
 
-// 起点终点
 const initTransformedKeyPoints = computed<Coordinate[]>(() => keyPoints.value.map(item => fromLonLat(item)))
 const keyPointFeatures = computed(() => initTransformedKeyPoints.value.map((item) => {
   const KeyPointFeature = new Feature({
@@ -56,7 +53,6 @@ const keyPointFeatures = computed(() => initTransformedKeyPoints.value.map((item
   })
   return KeyPointFeature
 }))
-// 途经点连成线
 const initTransformedPath = computed<Coordinate[]>(() => path.value.map(item => fromLonLat(item)))
 const lineFeature = new Feature({
   geometry: new LineString(initTransformedPath.value),
@@ -72,7 +68,6 @@ source.addFeature(lineFeature)
 const vector = new VectorLayer({
   source,
   style: (feature, _index) => {
-    // 线条样式
     const styles = [
       new Style({
         stroke: new Stroke({
@@ -84,7 +79,6 @@ const vector = new VectorLayer({
     const geometry = feature.getGeometry()!
     if (geometry instanceof LineString) {
       const coordinates = geometry.getCoordinates()
-      // 端点样式
       coordinates.forEach((item) => {
         styles.push(
           new Style({
@@ -102,7 +96,6 @@ const vector = new VectorLayer({
           }),
         )
       })
-      // 箭头样式
       for (let i = 0; i < coordinates.length - 1; i++) {
         const start = coordinates[i]
         const end = coordinates[i + 1]
@@ -129,7 +122,6 @@ const vector = new VectorLayer({
 const mapRef = ref(null)
 let map: Map | null = null
 
-// 添加绘制、修改和选择交互
 const draw = new Draw({
   source,
   type: 'LineString',
@@ -138,10 +130,7 @@ const modify = new Modify({
   deleteCondition: singleClick,
   source,
 })
-// 关键点移动结束之后，刷新路径点和关键点绑定的变量
-// 避免点重复渲染的Flag
 let flagModifyKeyPoint = false
-// 避免线重复渲染的Flag
 let flagModifyPath = false
 modify.on('modifyend', (event) => {
   let flagKeyPointChange = false
@@ -171,7 +160,6 @@ modify.on('modifyend', (event) => {
     path.value = lineFeature.getGeometry()!.getCoordinates().map(item => toLonLat(item))
   }
 })
-// 修改关键点后，刷新关键点渲染，同时刷新路径点
 watch(keyPoints, () => {
   if (flagModifyKeyPoint) {
     flagModifyKeyPoint = false
@@ -189,7 +177,6 @@ watch(keyPoints, () => {
 }, {
   deep: true,
 })
-// 修改路径点后，刷新路径点渲染
 watch(path, () => {
   if (flagModifyPath) {
     flagModifyPath = false
@@ -203,7 +190,6 @@ watch(path, () => {
   deep: true,
 })
 
-// 装载地图
 onMounted(() => {
   map = new Map({
     target: mapRef.value!,
@@ -216,7 +202,6 @@ onMounted(() => {
   map.addInteraction(modify)
 })
 
-// 操作按钮
 function drawLine() {
   map!.removeInteraction(modify)
   map!.addInteraction(draw)
@@ -231,15 +216,13 @@ function cancelLine() {
 }
 function addPoint() {
   keyPoints.value.push([116.4074, 39.9042])
-  // path.value.push([16.4074, 39.9042])
 }
 function removePoint() {
   keyPoints.value.pop()
 }
 
-// For fun
 function getRandomArrayElements(arr: Array<any>) {
-  const count = Math.floor(Math.random() * (arr.length - 2)) + 3 // 随机数量，确保大于2
+  const count = Math.floor(Math.random() * (arr.length - 2)) + 3
   const shuffled = arr.slice(0)
   let i = arr.length
   const min = i - count
